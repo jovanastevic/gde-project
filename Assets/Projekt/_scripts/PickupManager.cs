8d7a8bf2901af56d2a02a5c9cbe78b95e3b4c419
Avoqato2 <mt241087@fhstp.ac.at>
2026-01-20 19:26:44 +0100

    KingBobomb Mountains and stuff

--- Assets/Projekt/_scripts/PickupManager.cs
+++ Assets/Projekt/_scripts/PickupManager.cs
@@ -34,9 +34,9 @@ public class PickupManager : MonoBehaviour
     private Pickup CreatePickup()
     {
         float[] possiblePos = { -5f, 0, 5f };
-
+ 
         float playerPosZ = Player.transform.position.z + 15f;
-        Vector3 targetPosition = new Vector3(possiblePos[Random.Range(0,3)], 1, playerPosZ);
+        Vector3 targetPosition = new Vector3(possiblePos[Random.Range(0,3)], Player.transform.position.y, playerPosZ);
         Pickup pickup = pickupPool.GetPoolObjekt();
         pickup.transform.position = targetPosition;
         return pickup;
